9144773abf3ee2c6fee56a793440fcf2ca42cfdb
Marcelo Baptista <marcelobaptista@outlook.com>
2019-03-31 18:18:34 -0300

    update script

---
+++ centos7/centos_desktop.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Adicionando repositórios extras
+#
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+yum install -y https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm https://centos7.iuscommunity.org/ius-release.rpm yum-utils
+#
+# Atualização inicial do sistema
+#
+yum update -y
+#
+# Instalando interface gráfica e XFCE
+#
+yum groupinstall "X Window System" -y
+yum groupinstall "Xfce" -y
+rm /usr/share/xsessions/openbox.desktop
+systemctl set-default graphical.target
+#
+# Instalando pacotes básicos e removendo os desnecessários
+#
+yum remove firewalld postfix -y
+yum install -y alsa-utils alsa-plugins-pulseaudio autoconf automake bash-completion bc bzip2 certmonger evince file-roller firefox flash-plugin gcc git gnome-disk-utility gparted gtk3-devel gvfs-fuse gvfs-mtp htop light-locker mousepad mtr nano nautilus nautilus-image-converter network-manager-applet nmap ntfs-3g ntp numix-gtk-theme ristretto system-config-printer tcpdump traceroute unrar unzip vlc x264 xarchiver xdg-user-dirs xfce4-screenshooter xfce4-whiskermenu-plugin xvidcore wget
+yum install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sh
+wget "https://sourceforge.net/projects/openofficeorg.mirror/files/4.1.6/binaries/pt-BR/Apache_OpenOffice_4.1.6_Linux_x86-64_install-rpm_pt-BR.tar.gz/download" -O Apache_OpenOffice_4.1.6_Linux_x86-64_pt-BR.tar.gz
+tar -xvf Apache_OpenOffice_4.1.6_Linux_x86-64_pt-BR.tar.gz
+rpm -Uvih pt-BR/RPMS/*.rpm
+rpm -Uvih pt-BR/RPMS/desktop-integration/openoffice4.1.6-redhat-menus-4.1.6-9790.noarch.rpm
+yum clean all
+#
+# Aplicando configurações adicionais
+#
+sed -i 's/^SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+systemctl restart sshd
+mkdir /etc/xdg/xfce4/kiosk
+cat << "EOF">>  /etc/xdg/xfce4/kiosk/kioskrc
+[xfce4-session]
+SaveSession=NONE
+EOF
+#
+# Habilitando o NTP e desabilitando serviços desnecessários
+# 
+systemctl disable lvm2-lvpolld.socket lvm2-lvmetad.socket lvm2-monitor.service
+systemctl enable ntpd 
+#
+# Instalando Kernel LTS mais recente
+#
+yum --enablerepo=elrepo-kernel install -y kernel-lt
+grub2-set-default 0 && grub2-mkconfig -o /boot/grub2/grub.cfg
+#
+# Limpando histórico do Shell e reiniciando
+#
+cat /dev/null > ~/.bash_history && history -c && reboot
